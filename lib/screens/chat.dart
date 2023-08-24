fdfaa76cdbeb17949974651c9acdb7b9201cb73d
ebramfadl <ebram@ntsal.com>
2023-08-25 02:45:35 +0300

    working on chat screen

---
+++ lib/screens/chat.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final firestore = FirebaseFirestore.instance;
+FirebaseUser loggedInUser;
+
+class Chat extends StatefulWidget{
+
+  static const String id = 'chat';
+
+  @override
+  State<StatefulWidget> createState() {
+    return ChatState();
+  }
+}
+
+class ChatState extends State<Chat>{
+
+  final messageTextController = TextEditingController();
+  final auth = FirebaseAuth.instance;
+  late String messageText;
+
+  @override
+  void initState(){
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async{
+    try{
+      final user = await auth.currentUser;
+      if(user != null){
+        loggedInUser = user;
+      }
+    }
+    catch(e){
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: (){
+              auth.signOut();
+              Navigator.pop(context);
+            },
+          ),
+        ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MessagesStream(),
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: messageTextController,
+                      onChanged: (value){
+                        messageText = value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      messageTextController.clear();
+                      firestore.collection('messages').add({
+                        'text' : messageText,
+                        'sender' : loggedInUser.email,
+                      });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
